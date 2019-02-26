ba080a12769dbcedd699d7dece8e7514b19aa36a
Frodeluren <frodeluren@gmail.com>
2019-02-26 14:06:40 +0100

    Initialized spring application
    This is just generated with https://start.spring.io/

---
+++ src/main/java/com/StudEval/StudEvalNoFuzzy/StudEvalNoFuzzyApplication.java
@@ -0,0 +1,13 @@
+package com.StudEval.StudEvalNoFuzzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudEvalNoFuzzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudEvalNoFuzzyApplication.class, args);
+	}
+
+}
