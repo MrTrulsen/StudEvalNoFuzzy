b9b9523779c0d3583dac33eb0ebfda388d933282
Ørjan <orjantrulsensvendsen@gmail.com>
2019-03-07 13:24:01 +0100

    editet repository

--- src/main/java/com/StudEval/StudEvalNoFuzzy/Repositories/RepositoryTool.java
+++ src/main/java/com/StudEval/StudEvalNoFuzzy/Repositories/RepositoryTool.java
@@ -1,4 +1,26 @@
 package com.StudEval.StudEvalNoFuzzy.Repositories;
 
+import com.StudEval.StudEvalNoFuzzy.Evaluation.Answer;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class RepositoryTool {
+    private final JdbcTemplate jdbcTemplate;
+
+
+    public RepositoryTool(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Answer> findAllAnswersInDb(){
+        List<Answer> answers = new ArrayList<>();
+
+        return null;
+        //temp
+        // jdbcTemplate.query("SELECT * FROM books",);
+    }
 }
