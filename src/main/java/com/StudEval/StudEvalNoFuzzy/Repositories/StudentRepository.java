f350eb4fbac123133b7464ed7890c80c66c22a19
Ørjan Svendsen Trulsen <orjantrulsensvendsen@gmail.com>
2019-03-10 16:10:27 +0100

    Made repository classes for stud/teach. Also made basic rowmapper

---
+++ src/main/java/com/StudEval/StudEvalNoFuzzy/Repositories/StudentRepository.java
@@ -0,0 +1,25 @@
+package com.StudEval.StudEvalNoFuzzy.Repositories;
+
+import com.StudEval.StudEvalNoFuzzy.Evaluation.Question;
+import com.StudEval.StudEvalNoFuzzy.RowMappers.QuestionRowMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentRepository {
+    private final JdbcTemplate jdbcTemplate;
+    private QuestionRowMapper questionRowMapper = new QuestionRowMapper();
+
+    @Autowired
+    public StudentRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Question> FindAllQuestions(){
+        List<Question> questions = new ArrayList<>();
+        return jdbcTemplate.query("SELECT * FROM questions", questionRowMapper);
+    }
+
+}
