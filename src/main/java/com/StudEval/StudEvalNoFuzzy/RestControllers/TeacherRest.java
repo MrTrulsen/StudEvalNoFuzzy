5e8ad094f410e2d915daca1c270a12f8276d65f9
Ørjan <orjantrulsensvendsen@gmail.com>
2019-03-11 11:02:51 +0100

    added RESTcontrollers

---
+++ src/main/java/com/StudEval/StudEvalNoFuzzy/RestControllers/TeacherRest.java
@@ -0,0 +1,7 @@
+package com.StudEval.StudEvalNoFuzzy.RestControllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeacherRest {
+}
--- src/main/java/com/StudEval/StudEvalNoFuzzy/StudEvalNoFuzzyApplication.java
+++ src/main/java/com/StudEval/StudEvalNoFuzzy/StudEvalNoFuzzyApplication.java
@@ -3,7 +3,7 @@ package com.StudEval.StudEvalNoFuzzy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication( scanBasePackages = { "com.StudEval.StudEvalNoFuzzy.Repositories", "com.StudEval.StudEvalNoFuzzy.config"})
+@SpringBootApplication( scanBasePackages = { "com.StudEval.StudEvalNoFuzzy.Repositories", "com.StudEval.StudEvalNoFuzzy.config","com.StudEval.StudEvalNoFuzzy.RestControllers"})
 public class StudEvalNoFuzzyApplication {
 
 	public static void main(String[] args) {
