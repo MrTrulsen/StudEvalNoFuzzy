5d5ffeb62b8082bbb6861d9673d00617c50eafb7
Frodeluren <frodeluren@gmail.com>
2019-03-12 13:43:26 +0100

    Implemented user system and possibility to log in/out for teachers
    A sql file is placed in the root folder

--- src/main/java/com/StudEval/StudEvalNoFuzzy/config/Security.java
+++ src/main/java/com/StudEval/StudEvalNoFuzzy/config/Security.java
@@ -1,38 +1,83 @@
 package com.StudEval.StudEvalNoFuzzy.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @Configuration
 public class Security extends WebSecurityConfigurerAdapter {
 
+
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Value("${spring.queries.users-query}")
+    private String usersQuery;
+
+    @Value("${spring.queries.roles-query}")
+    private String rolesQuery;
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication()
-                .withUser("frode").password("password420").roles("STUDENT").roles("USER")
+        auth.jdbcAuthentication().usersByUsernameQuery(usersQuery).authoritiesByUsernameQuery(rolesQuery)
+                .dataSource(dataSource).passwordEncoder(passwordEncoder());
+                /*.withUser("frode@ntnu.no").password(passwordEncoder().encode("password420")).authorities("STUDENT")
                 .and()
-                .withUser("arne").password("password69").roles("TEACHER").roles("USER");
+                .withUser("arne").password("password69").roles("TEACHER");*/
     }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
                 .authorizeRequests()
-                .antMatchers("/teacherpage**").permitAll()//.hasRole("TEACHER")     // TODO: remove permitAll when done testing
-                .antMatchers("/studentpage**").permitAll()//.hasRole("STUDENT")     // TODO: remove permitAll when done testing
-                .antMatchers("/index**").permitAll()
-                .antMatchers("/login**").permitAll()
-                .antMatchers("/teacherlogin**").permitAll()
-                .antMatchers("/studentlogin**").permitAll();
-                //.anyRequest()
-                //.permitAll()
-                //.and()
-                //.httpBasic();
-                //.formLogin();
-        httpSecurity.csrf().disable();
+                .antMatchers("/").permitAll()
+                .antMatchers("/teacherlogin").permitAll()
+                .antMatchers("/register").permitAll()
+
+                .antMatchers("/teacherpage").hasAuthority("TEACHER_USER")
+                .anyRequest().authenticated()
+                .and()
+
+                //form login
+                .csrf().disable().formLogin()
+                .loginPage("/teacherlogin")
+                .failureUrl("/login?error=true")
+                .defaultSuccessUrl("/teacherpage")
+
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .and()
+                //logout
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/").and()
+                .exceptionHandling()
+                .accessDeniedPage("/access-denied");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**", "/bootstrap/**");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
+
+
