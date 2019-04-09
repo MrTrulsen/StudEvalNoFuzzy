acaf2ec5d8d26f38a777354518e1427366a4a4fe
Ørjan <orjantrulsensvendsen@gmail.com>
2019-04-09 12:25:33 +0200

    created mainrep instead of two

--- src/main/java/com/StudEval/StudEvalNoFuzzy/Repositories/MainRepository.java
+++ src/main/java/com/StudEval/StudEvalNoFuzzy/Repositories/MainRepository.java
@@ -1,6 +1,10 @@
 package com.StudEval.StudEvalNoFuzzy.Repositories;
 
+import com.StudEval.StudEvalNoFuzzy.Evaluation.Answer;
+import com.StudEval.StudEvalNoFuzzy.Evaluation.Evaluation;
 import com.StudEval.StudEvalNoFuzzy.Evaluation.Question;
+import com.StudEval.StudEvalNoFuzzy.User.Role;
+import com.StudEval.StudEvalNoFuzzy.User.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,14 +29,15 @@ public class MainRepository {
 
     /**
      * Fetches the related questions to the right course
+     *
      * @param evalId
      * @return The question with this id.
      */
-    public List findRelatedQuestionsToEval(Integer evalId){
+    public List findRelatedQuestionsToEval(Integer evalId) {
         List<Question> questions = new ArrayList<>();
         String query = "SELECT q.* FROM questions q INNER JOIN eval_ques_junc j ON q.question_id = j.question_id WHERE eval_id=?";
-        SqlRowSet rs = jdbcTemplate.queryForRowSet(query, new Object[] {evalId});
-        if(rs.first()){
+        SqlRowSet rs = jdbcTemplate.queryForRowSet(query, new Object[]{evalId});
+        if (rs.first()) {
             Question question = new Question(rs.getInt("question_id"),
                     rs.getString("question_text"),
                     rs.getFloat("complexity"),
@@ -40,7 +45,7 @@ public class MainRepository {
                     rs.getFloat("difficulty"),
                     rs.getFloat("importance"));
             questions.add(question);
-            while(rs.next()){
+            while (rs.next()) {
                 question = new Question(rs.getInt("question_id"),
                         rs.getString("question_text"),
                         rs.getFloat("complexity"),
@@ -52,4 +57,193 @@ public class MainRepository {
         }
         return questions;
     }
+
+    /**
+     * Fetches the related questions
+     *
+     * @param evalId
+     * @return The question with this id.
+     */
+    public List findRelatedAnswersToCourseId(Integer evalId) {
+        List<Answer> answers = new ArrayList<>();
+        String query = "SELECT r.* FROM responses r INNER JOIN eval_ques_junc j ON r.question_id = j.question_id WHERE eval_id=?";
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(query, new Object[]{evalId});
+        if (rowSet.first()) {
+            Answer answer = new Answer(rowSet.getInt("answer_id"),
+                    rowSet.getInt("question_id"),
+                    rowSet.getFloat("complexity"),
+                    rowSet.getFloat("time_use"),
+                    rowSet.getFloat("difficulty"),
+                    rowSet.getFloat("importance"));
+            answers.add(answer);
+            while (rowSet.next()) {
+                answer = new Answer(rowSet.getInt("answer_id"),
+                        rowSet.getInt("question_id"),
+                        rowSet.getFloat("complexity"),
+                        rowSet.getFloat("time_use"),
+                        rowSet.getFloat("difficulty"),
+                        rowSet.getFloat("importance"));
+                answers.add(answer);
+            }
+        }
+        return answers;
+    }
+
+    /**
+     * Fetches the students(users with role id = 2) in given evaluation.
+     *
+     * @param evalId
+     * @return
+     */
+    public List<String> findStudentsInEvaluation(Integer evalId) {
+        List<String> studentsEmailsInCurrentEval = new ArrayList<>();
+
+        //Kan gjøre denne om denne query senere for å få ut eventuelle lærere til emne
+        String query = "SELECT u.* FROM user u " +
+                "INNER JOIN user_role r ON u.user_id = r.user_id " +
+                "INNER JOIN eval_user_junc e ON u.user_id = e.user_id " +
+                "WHERE role_id=2 AND eval_id=?";
+
+        SqlRowSet rs = jdbcTemplate.queryForRowSet(query, new Object[]{evalId});
+        while (rs.next()) {
+            String userEmail = new String(rs.getString("email"));
+            studentsEmailsInCurrentEval.add(userEmail);
+        }
+        return studentsEmailsInCurrentEval;
+    }
+
+    /**
+     * Finding all the current users in db
+     *
+     * @return List of users in db
+     */
+    public List<String> findUserEmailsInDb() {
+        List<String> userEmails = new ArrayList<>();
+        String query = "SELECT user_id, email, is_active FROM user";
+        SqlRowSet rs = jdbcTemplate.queryForRowSet(query);
+        while (rs.next()) {
+            String user = (rs.getString("email"));
+            userEmails.add(user);
+        }
+        return userEmails;
+    }
+
+
+    /**
+     * Importing students to the actual course.
+     *
+     * @param users
+     * @param evalId
+     * @return null if success, if not success "Could not add students."
+     */
+    public String importStudentsToEvaluation(List<User> users, Integer evalId) {
+        List<String> userEmailsInDb = findUserEmailsInDb();
+        int numRows = 0;
+        for (User user : users) {
+            if (!userEmailsInDb.contains(user.getEmail())) {
+                //Then add student
+                user.setStatus(0);
+                Role role = new Role();
+                role.setId(2);
+                addUser(user, role);
+                user.setId(getNewUserId(user.getEmail()));
+            }
+            String query = "INSERT INTO eval_user_junc(user_id,eval_id) VALUES(?,?)";
+
+            try {
+                user.setId(getNewUserId(user.getEmail()));
+                numRows = jdbcTemplate.update(query, user.getId(), evalId);
+
+            } catch (Exception ex) {
+                return "Could not add students: " + ex.getMessage();
+            }
+        }
+        if (numRows == 1) {
+            return null;
+        } else {
+            return "Could not add students.";
+        }
+    }
+
+    /**
+     * Adds a user in db if the user have not registered yet.
+     *
+     * @param user
+     */
+    public void addUser(User user, Role role) {
+        String query = "INSERT INTO user (email,is_active) VALUES (?,?)";
+        jdbcTemplate.update(query, user.getEmail(), user.getStatus());
+        Integer newId = getNewUserId(user.getEmail());
+        String query2 = "INSERT INTO user_role (user_id,role_id) VALUES (?,?)";
+        jdbcTemplate.update(query2, newId, role.getId());
+    }
+
+    /**
+     * Gets the new user_id of requested user recently added.
+     *
+     * @param email
+     * @return user id of requested email
+     */
+    public int getNewUserId(String email) {
+        String query = "SELECT user_id FROM user WHERE email=?";
+        Integer user_id;
+        user_id = jdbcTemplate.queryForObject(query, new Object[]{email}, Integer.class);
+        return user_id;
+    }
+
+
+    /**
+     * Adds new evaluation
+     *
+     * @param evaluation
+     * @return null if success, "Could not add evaluation" else
+     */
+    public String addNewEvaluation(Evaluation evaluation, String courseName) {
+        String query = "INSERT INTO evaluation (date_start,date_stop, course_id, time_of_exam) VALUES (?,?,?,?)";
+        List<String> courseIds = getAllCourseIds();
+        Integer numRows;
+
+        if (!courseIds.contains(evaluation.getCourseId())) {
+            addCourse(evaluation.getCourseId(), courseName);
+        }
+        numRows = jdbcTemplate.update(query, evaluation.getStartDate(), evaluation.getStopDate(), evaluation.getCourseId(), evaluation.getTimeOfExam());
+        if (numRows == 1) {
+            return null;
+        } else {
+            return "Could not add evaluation";
+        }
+    }
+
+    /**
+     * Adds a course
+     *
+     * @param courseName
+     */
+    private void addCourse(String courseId, String courseName) {
+        String query = "INSERT INTO course (course_id,name) VALUES (?,?)";
+        try {
+            jdbcTemplate.update(query, courseId, courseName);
+        } catch (Exception ex) {
+            throw ex;
+        }
+
+    }
+
+    /**
+     * Fetches all the course IDs
+     *
+     * @return
+     */
+    private List<String> getAllCourseIds() {
+        String query = "SELECT course_id FROM course";
+        List<String> courseIds = new ArrayList<>();
+        SqlRowSet rs = jdbcTemplate.queryForRowSet(query);
+        while (rs.next()) {
+            String courseId = (rs.getString("course_id"));
+            courseIds.add(courseId);
+        }
+        return courseIds;
+    }
+
+
 }
--- src/main/java/com/StudEval/StudEvalNoFuzzy/RestControllers/StudentRest.java
+++ src/main/java/com/StudEval/StudEvalNoFuzzy/RestControllers/StudentRest.java
@@ -2,7 +2,6 @@ package com.StudEval.StudEvalNoFuzzy.RestControllers;
 
 import com.StudEval.StudEvalNoFuzzy.Evaluation.Question;
 import com.StudEval.StudEvalNoFuzzy.Repositories.MainRepository;
-import com.StudEval.StudEvalNoFuzzy.Repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,23 +12,16 @@ import java.util.List;
 @RestController
 public class StudentRest {
 
-    private final StudentRepository studentRepository;
     private final MainRepository mainRepository;
 
 
     @Autowired
-    public StudentRest(StudentRepository studentRepository, MainRepository mainRepository) {
-        this.studentRepository = studentRepository;
+    public StudentRest(MainRepository mainRepository) {
         this.mainRepository = mainRepository;
     }
 
-    //Test for getting all the questions
-    @RequestMapping("/allQuestions")
-    public List<Question> listAllQuestions(){
-        return studentRepository.findAllQuestions();
-    }
 
-    @RequestMapping("/.")
+    @RequestMapping("/getQuestionsInEval")
     public List<Question> listRelatedQuestions(){
         //TO DO; implement course_ID to come in
         Integer evalId = 1;
