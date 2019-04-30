53adfde68a655a1c695552899f2faf108846ad80
Kristian <Kristianoren@yahoo.com>
2019-04-30 14:17:11 +0200

    fixed bug

--- src/main/resources/static/js/addStudent.js
+++ src/main/resources/static/js/addStudent.js
@@ -1,29 +1,22 @@
-function generateStudent(){
-    var email = document.getElementById("studentEmailInput").value;
-    var password= null;
 
-    var student = {
-        email: email,
-        password: password
-    };
+
 
 console.log(student);
-}
-
 //Adds students to the database
 function addStudent(users) {
     console.log("Adding student...");
-    fetch("/addStudent/" ,{
+    fetch("/addStudent/606069" ,{
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(users)
-    }).then(function (response, btn) {
-        btn = document.getElementById("addStudentsBtn"); //Temporary fix
+    }).then(function (response) {
+       var btn = document.getElementById("addStudentsBtn"); //Temporary fix
         console.log("Response: ", response);
         if (response.status === 200) {
             btn.setAttribute("data-dismiss", "modal");
+            generateStudent(user);
 
         } else {
             btn.removeAttribute("data-dismiss", "modal");
@@ -31,5 +24,34 @@ function addStudent(users) {
         }
             }).then(function () {
         showErrorMessage("modalAddEvalBody", "Error when trying to add student. Please try again.");
+
     });
 }
+function generateStudent(){
+
+        var email = document.getElementById("studentEmailInput").value;
+        var password= null;
+
+        var student = {
+            email: email,
+            password: password
+        };
+
+console.log(student);
+
+//checkForExistingStudent(text,email,password,btn);
+
+
+function checkForExistingStudent(text, email, password, btn) {
+
+    if (user != null) {
+
+        console.log(student + "already exists!");
+
+        btn.removeAttribute("data-dismiss", "modal");
+    }
+    else {
+        btn.setAttribute("data-dismiss", "modal");
+        addStudent(users);
+    }
+}}
